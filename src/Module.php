b45e28e85c5be1da2ac56214754c3338a1b9367c
Austen McDonald <austenmc@fb.com>
2016-08-20 14:15:02 -0700

    Update to latest API

--- src/Module.php
+++ src/Module.php
@@ -5,9 +5,10 @@ namespace MyVendor\MyNamespace;
 
 use LotGD\Core\Game;
 use LotGD\Core\Module as ModuleInterface;
+use LotGD\Core\Models\Module as ModuleModel;
 
 class Module implements ModuleInterface {
     public static function handleEvent(Game $g, string $event, array $context) { }
-    public static function onRegister(Game $g) { }
-    public static function onUnregister(Game $g) { }
+    public static function onRegister(Game $g, ModuleModel $module) { }
+    public static function onUnregister(Game $g, ModuleModel $module) { }
 }
