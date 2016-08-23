10dd9d5c09e6a58ebc71059fdf23de1401d4f594
Austen McDonald <austen.mcdonald@gmail.com>
2016-08-23 21:44:49 +0000

    Fix handleEvent callsite to match latest spec

--- src/Module.php
+++ src/Module.php
@@ -8,7 +8,7 @@ use LotGD\Core\Module as ModuleInterface;
 use LotGD\Core\Models\Module as ModuleModel;
 
 class Module implements ModuleInterface {
-    public static function handleEvent(Game $g, string $event, array $context) { }
+    public static function handleEvent(Game $g, string $event, array &$context) { }
     public static function onRegister(Game $g, ModuleModel $module) { }
     public static function onUnregister(Game $g, ModuleModel $module) { }
 }
