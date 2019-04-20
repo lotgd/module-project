28bb0043d5d83bc75536c8a7f9c7ba5d693165fd
Basilius Sauter <basilius.sauter@gmail.com>
2019-04-20 12:36:22 +0200

    Added compability for lotgd/core 0.5

--- tests/ModuleTest.php
+++ tests/ModuleTest.php
@@ -1,64 +1,17 @@
 <?php
 declare(strict_types=1);
 
-use Monolog\Logger;
-use Monolog\Handler\NullHandler;
+namespace MyVendor\MyNamespace\Tests;
 
-use LotGD\Core\Configuration;
 use LotGD\Core\Game;
+use LotGD\Core\Models\Character;
 use LotGD\Core\Models\Module as ModuleModel;
-use LotGD\Core\Tests\ModelTestCase;
 
 use MyVendor\MyNamespace\Module;
 
-class ModuleTest extends ModelTestCase
+class ModuleTest extends ModuleTestCase
 {
-    const Library = 'lotgd/module-project';
-
-    public $g;
-    private $moduleModel;
-
-    protected function getDataSet(): \PHPUnit_Extensions_Database_DataSet_YamlDataSet
-    {
-        return new \PHPUnit_Extensions_Database_DataSet_YamlDataSet(implode(DIRECTORY_SEPARATOR, [__DIR__, 'datasets', 'module.yml']));
-    }
-
-    public function setUp()
-    {
-        parent::setUp();
-
-        // Make an empty logger for these tests. Feel free to change this
-        // to place log messages somewhere you can easily find them.
-        $logger  = new Logger('test');
-        $logger->pushHandler(new NullHandler());
-
-        // Create a Game object for use in these tests.
-        $this->g = new Game(new Configuration(getenv('LOTGD_TESTS_CONFIG_PATH')), $logger, $this->getEntityManager(), implode(DIRECTORY_SEPARATOR, [__DIR__, '..']));
-
-        // Register and unregister before/after each test, since
-        // handleEvent() calls may expect the module be registered (for example,
-        // if they read properties from the model).
-        $this->moduleModel = new ModuleModel(self::Library);
-        $this->moduleModel->save($this->getEntityManager());
-        Module::onRegister($this->g, $this->moduleModel);
-
-        $this->g->getEntityManager()->flush();
-        $this->g->getEntityManager()->clear();
-    }
-
-    public function tearDown()
-    {
-        $this->g->getEntityManager()->flush();
-        $this->g->getEntityManager()->clear();
-
-        parent::tearDown();
-
-        Module::onUnregister($this->g, $this->moduleModel);
-        $m = $this->getEntityManager()->getRepository(ModuleModel::class)->find(self::Library);
-        if ($m) {
-            $m->delete($this->getEntityManager());
-        }
-    }
+    protected $dataset = "module";
 
     // TODO for LotGD staff: this test assumes the schema in their yaml file
     // reflects all columns in the core's models of characters, scenes and modules.
@@ -73,10 +26,16 @@ class ModuleTest extends ModelTestCase
         // Assert that databases are the same before and after.
         // TODO for module author: update list of tables below to include the
         // tables you modify during registration/unregistration.
-        $after = $this->getConnection()->createDataSet(['characters', 'scenes', 'modules']);
+        $tableList = [
+            'characters', 'scenes', 'modules',
+        ];
+
+        $after = $this->getConnection()->createDataSet($tableList);
         $before = $this->getDataSet();
 
-        $this->assertDataSetsEqual($before, $after);
+        foreach($tableList as $table) {
+            $this->assertSame($before->getTable($table)->getRowCount(), $after->getTable($table)->getRowCount());
+        }
 
         // Since tearDown() contains an onUnregister() call, this also tests
         // double-unregistering, which should be properly supported by modules.
